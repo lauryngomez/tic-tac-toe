b207c8264e95de30fed43c24b8f80d93a57e7985
Lauryn Gomez <lauryn@cmsllcnw.com>
2020-06-09 09:21:31 -0700

    board_spec.rb method test for placed

--- spec/board_spec.rb
+++ spec/board_spec.rb
@@ -12,6 +12,10 @@ describe 'A Tic Tac Toe board' do
     expect(board).to respond_to(:locations)
   end
 
+  it 'specifies location where token is placed' do
+    expect(board).to respond_to(:locations)
+  end
+
   it 'checks if it is empty' do
     expect(board).to be_empty
   end
