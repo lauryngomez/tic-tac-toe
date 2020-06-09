fe79d4697a95871730342feeb8f530762c204506
Lauryn Gomez <lauryn@cmsllcnw.com>
2020-06-09 09:15:15 -0700

    board_spec.rb let lock for Board.new

--- spec/board_spec.rb
+++ spec/board_spec.rb
@@ -2,8 +2,19 @@ require_relative '../lib/board'
 
 describe 'A Tic Tac Toe board' do
 
+  let (:board) { Board.new }
+
   it 'exists' do
-    Board.new
+    board
   end
 
+  # it 'places token' do
+  #   Board.new.place([1,2], 1, 2)
+  # end
+
+  # it 'checks if it is empty' do
+  #   pass
+  # end
+
+ 
 end
